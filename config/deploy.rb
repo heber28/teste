ac6daa43c1f1178ff15c8e28b1ab6df4a29ee510
Heber Massami Nunomura <heber28@gmail.com>
2011-08-09 11:12:33 -0300

    testando mudança para mysql

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :use_sudo, false
 set :deploy_to, "/home/#{user}/#{application}"
 
 server domain, :app, :web, :db, :primary => true
-after "deploy:update_code", "deploy:custom_symlinks"
+#after "deploy:update_code", "deploy:custom_symlinks"
 
 namespace :deploy do
   task :start do ; end
@@ -17,10 +17,10 @@ namespace :deploy do
   task :restart, :roles => :app, :except => { :no_release => true } do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
   end
-  task :custom_symlinks do
-    run "rm -rf #{release_path}/db/production.sqlite3"
-    run "ln -s #{shared_path}/production.sqlite3 #{release_path}/db/production.sqlite3"
-  end
+  #task :custom_symlinks do
+  #  run "rm -rf #{release_path}/db/production.sqlite3"
+  #  run "ln -s #{shared_path}/production.sqlite3 #{release_path}/db/production.sqlite3"
+  #end
 end
 
 
